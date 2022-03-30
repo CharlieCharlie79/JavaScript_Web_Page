51394a630c50ffbbe1c0c1e807cc12fa3b0a8aa4
CharlieCharlie79 <carloswork067@hotmail.com>
2022-03-30 02:24:10 -0400

    Counter

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,20 @@
-//Nav bar function
-`$(function () {
-    $("a").click(function(e){
-        e.preventDefault();
-        $("a").removeClass("active");
-        $(this).addClass("active");
-    });
-});`
+/* Count down */
+
+var countDownDate = new Date("Jan 1, 2023 00:00:01").getTime();
+
+var x = setInterval(function()
+{
+    var now = new Date().getTime();
+
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var min = Math.floor((distance % (1000 * 60 *60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 *60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = min;
+    document.getElementById("seconds").innerHTML = seconds;
+}, 1000);
