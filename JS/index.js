c1dcd8ada03c2e3c3dd618ccfa83554bad24ab32
CharlieCharlie79 <carloswork067@hotmail.com>
2022-04-03 07:04:03 -0400

    Weather app

--- JS/index.js
+++ JS/index.js
@@ -19,40 +19,38 @@ var x = setInterval(function()
 }, 1000);
 
 
-/* Weather */
-let lon;
-let lat;
-let temperature = document.querySelector(".temp");
-let summary = document.querySelector(".summary");
-let loc = document.querySelector(".location");
-let icon = document.querySelector(".icon");
-const kelvin = 273;
 
-window.addEventListener("load", () => {
-    if(navigator.geolocation)
-    {
-        navigator.geolocation.getCurrentPosition((position) =>{
-            console.log(position);
-            lon = position.coords.longitude;
-            lat = position.coords.latitude;
+/* Weather App */
 
-            const api = "768c56f3d272489471d1340bd1083e16"
+var inputval = document.querySelector('#cityinput');
+var btn = document.querySelector('#add');
+var city = document.querySelector("#cityoutput");
+var descrip = document.querySelector("#description");
+var temp = document.querySelector('#temp');
+var wind = document.querySelector("#wind");
 
-            const base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&` + `lon=${lon}&appid=768c56f3d272489471d1340bd1083e16`;
+api = "768c56f3d272489471d1340bd1083e16";
 
-            fetch(base)
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                console.log(data);
-                temperature.textContent = Math.floor(data.main.temp - kelvin) + "C";
-                summary.textContent = data.weather[0].description;
-                loc.textContent = data.name + "," + data.sys.country;
-                let icon1 = data.weather[0].icon;
-                icon.innerHTML = 
-                `<img src="IMG/icon_cloud.png" style = 'height:10rem'/>`;
-            })
-        })
-    }
+function convertion(val)
+{
+    return (val - 273).toFixed(2);
+}
+
+btn.addEventListener('click', function(){
+
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' +inputval.value+ "&appid=" +api).then(res => res.json())
+
+    .then(data => {
+        var nameval = data['name'];
+        var descrip = data['weather']['0']['description'];
+        var temperature = data['main']['temp'];
+        var wndspd = data['wind']['speed'];
+
+        city.innerHTML= `Weather of  <span>${nameval} <span>`;
+        temp.innerHTML = `Temperature: <span>${convertion(temperature)} C</span>`;
+        description.innerHTML = `Sky Conditions: <span> ${descrip}<span>`;
+        wind.innerHTML = `Wind Speed: <span>${wndspd} km/h <span>`
+    })
+
+    .catch(err => alert('You entered Wrong cityu name'));
 })
